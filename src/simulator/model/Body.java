ec1165cf477fe0c82d6d029e0a8654648c2d2916
Sergio <sramos02@ucm.es>
2019-02-04 12:01:15 +0100

    Primera sesion laboratorio
    Empezamos clase body

--- src/simulator/model/Body.java
+++ src/simulator/model/Body.java
@@ -1,4 +1,66 @@
 package simulator.model;
 
+import simulator.misc.Vector;
+
 public class Body {
+	private String id;
+	protected Vector v;
+	protected Vector a;
+	protected Vector p;
+	protected double m;
+	
+	
+	//CONSTRUCTOR 1
+	public Body(String id, Vector v, Vector a, Vector p, double m) {
+		this.id = id;
+		this.v = v;
+		this.a = a;
+		this.p = p;
+		this.m = m;
+	}
+	
+	
+	//FUNCIONES DEL PROGRAMA
+	public String getId() {
+		return this.id;
+	}
+	
+	public Vector getVelocity() {
+		return this.v;
+	}
+	
+	public Vector getPosition() {
+		return this.p;
+	}
+	
+	double getMass() {
+		return this.m;
+	}
+	
+	void setVelocity(Vector v) {
+		this.v = v;
+	}
+	
+	void setAceleration(Vector a) {
+		this.a = a;
+	}
+	
+	void setPosition(Vector p) {
+		this.p = p;
+	}
+	
+	void move(double t) {
+		//p += vt + 0.5at²
+		double aux = t*t;
+		Vector u = (v.scale(t));
+		Vector v = (a.scale(aux));
+		this.p = (this.p).plus(u).plus(v.scale(0.5));		//SUJETO A CAMBIOS  
+		
+		//v += at
+		this.v = (this.v).plus(a.scale(t));
+	}
+	
+	public String toString() {
+		return "id: " + this.id + ",mass: " + this.m + ",pos: " + this.p + ",vel: " + this.v + ",acc: " + this.a;
+	}
 }
